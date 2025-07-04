237a8ac56d1c1964eb62214e47bf1791cfd37b6f
EJ <ejloudalec13@gmail.com>
2025-07-04 23:12:59 +0800

    Fix: handled unused error variable in contact form

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+ 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["**/*.ts", "**/*.tsx"], // Target TypeScript files
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
-      "react/no-unescaped-entities": "off",       // Disable unescaped characters rule
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
-  },
-];
-
-export default eslintConfig;
+  }),
+]
+ 
+export default eslintConfig
