8581361650b045d1efdc2b5ee0d8d97e9470df54
EJ <ejloudalec13@gmail.com>
2025-07-05 17:19:09 +0800

    Fix: ESLint issues for unescaped characters and unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
-import { FlatCompat } from '@eslint/eslintrc'
- 
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Compatible alternative to import.meta.dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
- 
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
@@ -13,6 +18,6 @@ const eslintConfig = [
       '@next/next/no-page-custom-font': 'off',
     },
   }),
-]
- 
-export default eslintConfig
+];
+
+export default eslintConfig;
